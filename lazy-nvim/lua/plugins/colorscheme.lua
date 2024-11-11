f285a716a5e6d5680711ccd1094e7223a27f10cf
최형욱 <hw.choi@ridi.com>
2024-11-11 12:06:20 +0900

    chore: Fix lazy bootstrap option

--- lazy-nvim/lua/plugins/colorscheme.lua
+++ lazy-nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,32 @@
 return {
---  {
---    "shaunsingh/nord.nvim",
---    dependencies = { "akinsho/bufferline.nvim" },
---    lazy = false,
---    priority = 1000,
---    config = function()
---      local colors = require("nord.colors")
---      local highlights = require("nord").bufferline.highlights({
---        fill = colors.nord0_gui,
---        indicator = colors.nord9_gui,
---        bg = colors.nord0_gui,
---        buffer_bg = colors.nord0_gui,
---        buffer_bg_selected = colors.nord1_gui,
---        buffer_bg_visible = "#2A2F3A",
---        bold = true,
---        italic = false,
---      })
---
---      require("bufferline").setup({
---        options = {
---          separator_style = 'slant',
---        },
---        highlights = highlights,
---      })
---      -- load colorscheme
---      vim.cmd([[colorscheme nord]])
---    end
---  }
+  -- {
+  --   "shaunsingh/nord.nvim",
+  --   dependencies = { "akinsho/bufferline.nvim" },
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     local colors = require("nord.colors")
+  --     local highlights = require("nord").bufferline.highlights({
+  --       fill = colors.nord0_gui,
+  --       indicator = colors.nord9_gui,
+  --       bg = colors.nord0_gui,
+  --       buffer_bg = colors.nord0_gui,
+  --       buffer_bg_selected = colors.nord1_gui,
+  --       buffer_bg_visible = "#2A2F3A",
+  --       bold = true,
+  --       italic = false,
+  --     })
+
+  --     require("bufferline").setup({
+  --       options = {
+  --         separator_style = 'slant',
+  --       },
+  --       highlights = highlights,
+  --     })
+  --     -- load colorscheme
+  --     vim.cmd([[colorscheme nord]])
+  --   end
+  -- },
   {
     "zenbones-theme/zenbones.nvim",
     dependencies = "rktjmp/lush.nvim",
