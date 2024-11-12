be8f72f1b0216b3b8f132e3ddeb773c6833b1ba2
최형욱 <hw.choi@ridi.com>
2024-11-12 17:31:38 +0900

    chore(nvim-tree): Change lazy option

--- lazy-nvim/lua/plugins/file_explorer.lua
+++ lazy-nvim/lua/plugins/file_explorer.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    cmd = "NvimTreeToggle",
+    lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("nvim-tree").setup({
