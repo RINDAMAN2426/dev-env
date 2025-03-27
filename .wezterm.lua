13cc401c5c98104c1485104c943468288b3638bf
RINDAMAN2426 <brouk.develop@gmail.com>
2025-03-27 15:48:11 +0900

    chore: Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,28 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
+config.color_scheme = 'Ubuntu'
+config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font 'Monaco'
 config.window_decorations = 'RESIZE'
+
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' },
+  },
+}
 
 return config
